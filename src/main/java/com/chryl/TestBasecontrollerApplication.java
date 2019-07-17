2581274c38b388e484109ba8a1a982c2bbfce8bf
Chr0527 <1015670600@qq.com>
2019-07-17 14:55:05 +0800

    Initial commit

---
+++ src/main/java/com/chryl/TestBasecontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.chryl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBasecontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBasecontrollerApplication.class, args);
+	}
+
+}
